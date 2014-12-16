e9c4f61812472d1309be2b8e967c3856b3984c54
John Krasich <krasicji@rose-hulman.edu>
2014-12-15 22:39:16 -0500

    In order to correctly highlight keywords in the output display, we now utilize the nltk stemming that is done when determining the keywords.

--- src/ExactAbstract.py
+++ src/ExactAbstract.py
@@ -3,6 +3,7 @@ from flask import request
 from flask import render_template
 from algorithms.statistical import get_keyword
 from nltk.tokenize import word_tokenize
+from nltk.stem.porter import PorterStemmer
 import json
 
 app = Flask(__name__)
@@ -20,15 +21,23 @@ def get_keywords():
         return json.dumps(get_keyword(text))
     else:
         return 'Something has gone terribly wrong.'
-    
-    
+
+def get_highlighted_words(text, keys):
+    words = []
+    stemmer = PorterStemmer()
+    for word in text:
+        if stemmer.stem(word.lower()) in keys:
+            words.append(word)
+    return words
+
 @app.route('/keyword_output', methods=['POST'])
 def keyword_output():
     if request.method == 'POST':
         text = request.form['text']
         tokenized_text = word_tokenize(text)
         keywords = get_keyword(text)
-        return render_template('output.html', hashtags=keywords, text=text, tokenized_text=tokenized_text)
+        highlighted = get_highlighted_words(tokenized_text, keywords)
+        return render_template('output.html', hashtags=keywords, text=text, tokenized_text=tokenized_text, highlighted_text=highlighted)
     else:
         return 'Something has gone terribly wrong.'
 
