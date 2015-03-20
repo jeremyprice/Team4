21e7889cfb9ceba8b19cb09bf547a9f5cda5072f
rodriga <rodriga@rose-hulman.edu>
2015-03-20 14:11:18 -0400

    Users can no longer submit empty text on manual insert, they will get a warning and cannot move on until text is entered.

---
+++ src/static/js/homepage_handler.js
@@ -0,0 +1,18 @@
+/**
+ * Created by rodriga on 3/20/2015.
+ */
+$(function () {
+
+   $("#manualUploadBtn").click(function () {
+       if ($('#text').val() == "") {
+           alert('You must enter some text!');
+           return false;
+       }
+
+
+
+   });
+});
+
+
+
