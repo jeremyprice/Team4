9d01b75e99e16fc403bf64b5c0d112aa05b51235
rodriga <rodriga@rose-hulman.edu>
2015-03-24 13:19:16 -0400

    Added method to automatically submit searches when the user presses enter and made the text on the error page smaller

--- src/static/js/homepage_handler.js
+++ src/static/js/homepage_handler.js
@@ -3,15 +3,21 @@
  */
 $(function () {
 
-   $("#manualUploadBtn").click(function () {
-       if ($('#text').val() == "") {
-           alert('The textbox cannot be left blank, enter some text and try again.');
-           return false;
-       }
 
+    $(this).keydown(function (e) {
+        if (e.keyCode == '13') {
+            $("#keywordsubmitbtn").submit();
+        }
+    });
 
+    $("#manualUploadBtn").click(function () {
+        if ($('#text').val() == "") {
+            alert('The textbox cannot be left blank, enter some text and try again.');
+            return false;
+        }
 
-   });
+    });
+
 });
 
 
